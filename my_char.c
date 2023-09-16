c63bfd1821f79aa6f3abfaa3fec857c0776a26bc
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-16 22:02:26 +0300

    the basic function

---
+++ my_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * my_putchar - function that print char to stdout
+ * @c: the character to print.
+ *
+ * Return: on success one
+ */
+int my_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
