a2956f433e355c2654578305795f24ce418584ec
OMAREID0 <omareid221b@gmail.com>
2023-09-17 15:26:44 +0300

    craeting _printf.c and handler.c

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _printf - function to print whatever write in
+ * @format: array of string to print it on the screen
+ *
+ * Return: return number of printed charchters (counter)
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	list_t list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg, format);
+
+	printed_chars = handler(format, function_list, arg);
+	va_end(arg);
+	return (printed_chars);
+}
---
+++ handler.c
@@ -0,0 +1,40 @@
+#include "main.c"
+
+
+int handler(const char *format, list_t list[], va_list arg)
+{
+	int i, j, counter, value;
+
+	counter = 0;
+
+	for (i = 0; format[i] != '\0' ; i++)
+	{
+		if (format[i] == "%")
+		{
+			for (j = 0; list[j].spec != NULL ; j++)
+			{
+				if (format[i + 1] == list[j].spec[0])
+				{
+					value = list[j].fun(arg);
+					if (value == -1)
+						return (-1);
+					counter += value;
+					break;
+				}
+			}
+			if (list[j].spec == NULL)
+			{
+				my_putchar(format[i]);
+				my_putchar(format[i + 1]);
+				counter = counter + 2;
+			}
+			i = i + 1;
+		}
+		else
+		{
+			my_putchar(format[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
