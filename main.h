960f9f139f3eb95a01a69b18b0245d736bd69f2e
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-18 20:24:22 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,8 +7,8 @@
 
 struct function_list
 {
-	char *spec;
-	int (*fun)(va_list);
+  char *spec;
+  int (*fun)(va_list);
 };
 typedef struct function_list list_t;
 
@@ -16,7 +16,7 @@ int handler(const char *format, list_t list[], va_list arg);
 int _printf(const char *format, ...);
 int my_putchar(char);
 int print_string(va_list arg);
-int print_percent(va_list arg;
+int print_percent(va_list arg);
 int print_char(va_list arg);
 int print_decimal(va_list arg);
 
