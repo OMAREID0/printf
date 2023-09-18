0bbac6adb9a63b31b2acdf518641cafdfc5055fe
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-09-18 21:14:27 +0300

    Update helper.c

--- helper.c
+++ helper.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
 /**
- * base_len - calculate the length of num when it is converted to another base.
- * @num: numer that the function will work on.
- * @base: base to be calculated on.
- *
- * Return:the length of the number whent it is converted.
+ * rev_string - Reverses a string.
+ * @s: The input string to be reversed.
  */
-unsigned int base_len(unsigned int num, int base)
+void rev_string(char *s)
 {
-	unsigned int i;
+	int length = 0;
+	int start, end;
+	char temp;
 
-	for (i = 0; num > 0; ++i)
-		num /= base;
-	return (i);
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	start = 0;
+	end = length - 1;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
-
-/**
- * put_base - prints any pointer content.
- * @anything: content to print.
- */
-void put_base(char *anything)
-{
-	unsigned int i;
-
-	for (i = 0; anything; ++i)
-		my_putchar(anything[i]);
-}
-
 
