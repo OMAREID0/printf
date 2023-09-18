240c61614af012a608dfbe566afa9b415aed42b5
omar eid <omareid221b@gmail.com>
2023-09-19 01:05:46 +0300

    Update function_octal.c

--- function_octal.c
+++ function_octal.c
@@ -2,41 +2,48 @@
 
 /**
  * print_octal - print numbers in octal form
- * @arg: argument pass to function
+ * @arg: argument passed to the function
  *
- * Return: length of octal
+ * Return: length of octal representation
  */
 int print_octal(va_list arg)
 {
-	unsigned int num;
-	int len;
-	char *octal;
-	char *rev_str;
+    unsigned int num;
+    int len;
+    char *octal;
+    char *rev_str;
 
-	num = va_arg(arg, unsigned int);
+    num = va_arg(arg, unsigned int);
 
-	if (num == 0)
-		return (my_putchar('0'));
-	if (num < 1)
-		return (-1);
-	len = base_len(num, 8);
+    if (num == 0)
+        return (my_putchar('0'));
 
-	octal = malloc(sizeof(char) * len + 1);
-	if (octal == NULL)
-		return (-1);
-	for (len = 0; num > 0; len++)
-	{
-		octal[len] = (num % 8) + 48;
-		num = num / 8;
+    if (num < 1)
+        return (-1);
 
-	}
-	octal[len] = '\0';
-	rev_str = rev_string(octal);
-	if (rev_str == NULL)
-		return (-1);
+    len = base_len(num, 8);
 
-	put_base(rev_str);
-	free(octal_rep);
-	free(rev_str);
-	return (len);
+    octal = malloc(sizeof(char) * len + 1);
+    if (octal == NULL)
+        return (-1);
+
+    for (len = 0; num > 0; len++)
+    {
+        octal[len] = (num % 8) + '0'; // Use '0' instead of 48 for ASCII value
+        num = num / 8;
+    }
+    octal[len] = '\0';
+
+    rev_str = rev_string(octal);
+    if (rev_str == NULL)
+    {
+        free(octal);
+        return (-1);
+    }
+
+    put_base(rev_str);
+    free(octal);
+    free(rev_str);
+
+    return len; // Return the length of the octal representation
 }
