daa5f32900a183a7be9739698f1bfff8d61d5447
OMAREID0 <omareid221b@gmail.com>
2023-09-18 15:05:13 +0300

    edits

--- main.h
+++ main.h
@@ -12,7 +12,10 @@ struct function_list
 };
 typedef struct function_list list_t;
 
-int handler(const char *format, conver_t f_list[], va_list arg_list);
+int handler(const char *format, list_t list[], va_list arg);
 int _printf(const char *format, ...);
 int my_putchar(char);
-
+int print_string(va_list list);
+int print_percent(va_list list);
+int print_char(va_list list);
+int print_decimal(int var_list);
